cba52b08bde3769758ad13e5769bf64ed9746168
mehribon29 <ymehribon@gmail.com>
2025-05-19 18:54:19 +0500

    Update introsql.sql

--- lesson_1/homework/introsql.sql
+++ lesson_1/homework/introsql.sql
@@ -1,1 +1,12 @@
-i have many plans about my new job
+1. data is the raw facts. 2. database is a collection of data. 3. relational database is a connected group of data in different tables. table is structured format used in database
+1. can work with big GB, good storage, efficiency 2. security 3. fast 4. recovery 5. integration 
+there are two ways: 1. windows authentification 2. separate username and password 
+create table students (
+id INT,
+name VARCHAR(50),
+group_number INT PRIMARY KEY,
+age INT,
+grade CHAR NOT NULL
+)
+
+
